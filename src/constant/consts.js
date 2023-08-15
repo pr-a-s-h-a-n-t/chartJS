7a1b1d53f91822b62edc1057e7349d0c3e9c2f30
prashant mishra <prashantmishra111@outlook.com>
2023-08-15 18:01:43 +0530

    ss

---
+++ src/constant/consts.js
@@ -0,0 +1,26 @@
+export const menuItems = [
+  {
+    label: "HOME",
+    link: "/",
+  },
+  {
+    label: "ABOUT US",
+    link: "/About",
+  },
+  {
+    label: "SERVICES",
+    link: "/Services",
+  },
+  {
+    label: "MEMBERSHIP",
+    link: "/Memberships",
+  },
+  {
+    label: "FAQ",
+    link: "/FAQ",
+  },
+  {
+    label: "CONTACT US",
+    link: "/Contact-Us",
+  },
+];
