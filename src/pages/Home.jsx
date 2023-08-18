981592a4dffc55f7809e75646cc9ed060671925d
prashant mishra <prashantmishra111@outlook.com>
2023-08-18 17:30:58 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { menuItems } from "../constant/constant";
 import "./Home.css";
 
 const GptHome = () => {
-  const [showTypingAnimation, setShowTypingAnimation] = useState(false);
-
-  useEffect(() => {
-    // Delay showing the typing animation for a better visual effect
-    const animationTimeout = setTimeout(() => {
-      setShowTypingAnimation(true);
-    }, 1000); // Adjust the delay as needed
-
-    return () => clearTimeout(animationTimeout);
-  }, []);
-
-  const overlayStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "60%", // Adjust the width as needed
-    padding: "20px",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    color: "red",
-    textAlign: "center",
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    borderRight: showTypingAnimation ? "none" : "0.15em solid white",
-    animation: showTypingAnimation
-      ? "typing 1s steps(10, end), blink-caret 0.75s step-end infinite"
-      : "none",
-  };
   const [typedText, setTypedText] = useState("");
-  const [textIndex, setTextIndex] = useState(0);
   const textArray = [
-    "Hello coder..",
-    "Welcome to my website!",
-    "Type animation example.",
+    "Chat GPT",
+    "Let's Explore",
+    "Let's Discover",
+    "Let's Learn",
+    "Let's Build",
+    "Let's Grow",
   ];
-  const typingDelay = 100; // Delay between typing each character in milliseconds
+  const typingDelay = 100; // Adjust typing speed as needed
+  const delayBetweenTexts = 1000; // Adjust delay before moving to the next text
 
   useEffect(() => {
-    const currentText = textArray[textIndex];
-
     let currentIndex = 0;
+    let currentText = textArray[0]; // Start with the first text
 
     const typingInterval = setInterval(() => {
-      setTypedText(currentText.slice(0, currentIndex + 1));
+      setTypedText(
+        (prevTypedText) => prevTypedText + currentText[currentIndex]
+      );
       currentIndex++;
 
       if (currentIndex === currentText.length) {
@@ -54,16 +30,20 @@ const GptHome = () => {
 
         // Move to the next text in the array
         setTimeout(() => {
-          const nextIndex = (textIndex + 1) % textArray.length;
-          setTextIndex(nextIndex);
-        }, 5500); // Delay before moving to the next text
+          const nextIndex =
+            (textArray.indexOf(currentText) + 1) % textArray.length;
+          currentText = textArray[nextIndex];
+          currentIndex = 0;
+          setTypedText(""); // Reset typedText for the next animation
+        }, delayBetweenTexts);
       }
     }, typingDelay);
 
     return () => {
       clearInterval(typingInterval);
     };
-  }, [textArray, textIndex]);
+  }, [textArray]);
+
   return (
     <div>
       <nav>
